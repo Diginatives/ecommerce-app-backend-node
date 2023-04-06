d7c254200fab018ac8fe0c2aeaa5c03beb9b353c
Kashifdeveloper06 <Kashifdeveloper06>
2023-04-06 08:56:41 +0500

    mongoDb atlas

--- app.js
+++ app.js
@@ -32,7 +32,11 @@ const PORT = process.env.PORT || 5000;
 // Server Setup
 mongoose.set("strictQuery", false);
 mongoose
-  .connect("mongodb://localhost:27017/node-2FA")
+  // .connect("mongodb://localhost:27017/node-2FA")
+  .connect(process.env.mongooDb, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to mongodb"))
   .catch((error) => console.log(error));
 app.listen(PORT, () => {
